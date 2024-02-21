2d36c369d5c2d6f3510ff36e4d38db72a5749a18
NicolaeGilca <gilcaniku@gmail.com>
2024-02-21 09:58:45 +0200

    comment function

--- js/main.js
+++ js/main.js
@@ -34,3 +34,66 @@ closeMenuBtn.addEventListener("click", ()=>{
     
   })
 
+
+
+//comment section
+
+let userData = {
+  name: "Ilon Musk"
+}
+
+const getCommentInputBtn = document.getElementById("getComment");
+const commentsSection = document.getElementById("commentsSection");
+
+const commentInput = document.getElementById("commentInput");
+const addCommentBtn = document.getElementById("addCommentBtn");
+
+//comment bar
+getCommentInputBtn.addEventListener("click", ()=> {
+  document.querySelector(".feed__user-comment").classList.toggle("active");
+});
+
+//add comment func
+function commentCreating() {
+  let numOfComment = document.getElementById("comment-counter");
+  console.log(numOfComment.value)
+
+
+  if(commentInput === "" ){
+    return undefined;
+  }else {
+    let comment = document.createElement("li");
+    comment.innerHTML = userData.name + ": " + commentInput.value;
+    commentsSection.appendChild(comment);
+    let closeBtn = document.createElement("SPAN");
+    closeBtn.innerHTML = 'Remove comment'
+    comment.appendChild(closeBtn);
+
+  }
+  
+  commentInput.value = "";
+}
+//add comment from keydown
+commentInput.addEventListener("keydown", (event) => {
+  if (event.key === 'Enter' && commentInput.value != "") {
+  
+    commentCreating();
+    commentInput.blur()
+}
+});
+//add comment btn
+addCommentBtn.addEventListener("click", () => {
+  if(commentInput.value != "") {
+    commentCreating();
+    commentInput.blur();
+  }
+});
+//remove comment
+commentsSection.addEventListener("click",(e)=>{
+  if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+  }
+})
+
+
+// Comment counter
